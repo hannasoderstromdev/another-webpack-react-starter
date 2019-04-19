6d9763b87ef01e3c84f5702e071c6b6f3893fcb2
Hanna Söderström <gothbarbie84@gmail.com>
2019-04-19 16:18:43 +0200

    fix: Remove check for logged in

--- src/components/organisms/Navigation/Navigation.js
+++ src/components/organisms/Navigation/Navigation.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { NavLink, withRouter } from 'react-router-dom'
-import { connect } from 'react-redux'
 
 import Icon from 'components/atoms/Icon'
 
@@ -9,50 +8,30 @@ import styles from './navigation.module.scss'
 const isActive = (path, match, location) =>
   !!(match || path === location.pathname)
 
-const Navigation = ({ loggedIn }) => (
+const Navigation = () => (
   <nav className={styles.nav} data-testid="main-navigation">
-    {loggedIn ? (
-      <ul>
-        <li>
-          <NavLink exact isActive={isActive.bind(this, '/')} to="/">
-            <span>Home</span>
-            <Icon icon={['fas', 'list']} iconsize="1.8rem" />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink isActive={isActive.bind(this, '/private')} to="/private">
-            <span>Private</span>
-            <Icon icon={['fas', 'plus-circle']} iconsize="1.8rem" />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/settings">
-            <Icon icon={['fas', 'cog']} iconsize="1.8rem" />
-          </NavLink>
-        </li>
-      </ul>
-    ) : (
-      <ul>
-        <li className="disabled">
+    <ul>
+      <li>
+        <NavLink exact isActive={isActive.bind(this, '/')} to="/">
           <span>Home</span>
           <Icon icon={['fas', 'list']} iconsize="1.8rem" />
-        </li>
-        <li className="disabled">
+        </NavLink>
+      </li>
+      <li>
+        <NavLink isActive={isActive.bind(this, '/private')} to="/private">
           <span>Private</span>
           <Icon icon={['fas', 'plus-circle']} iconsize="1.8rem" />
-        </li>
-        <li className="disabled">
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/settings">
           <Icon icon={['fas', 'cog']} iconsize="1.8rem" />
-        </li>
-      </ul>
-    )}
+        </NavLink>
+      </li>
+    </ul>
   </nav>
 )
 
-const mapStateToProps = ({ user }) => ({
-  loggedIn: user && user.loggedIn,
-})
-
 // NOTE! isActive only works if withRouter is the outermost HOC,
 // see this for more details: https://reacttraining.com/react-router/web/guides/dealing-with-update-blocking
-export default withRouter(connect(mapStateToProps)(Navigation))
+export default withRouter(Navigation)
