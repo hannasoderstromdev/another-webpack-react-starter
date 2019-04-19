23733bd1975f5f00d5ca632da4f5024247ebeb86
Hanna Söderström <gothbarbie84@gmail.com>
2019-04-19 15:30:55 +0200

    feat: add module scss

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,11 +3,13 @@ import React from 'react'
 import Page from 'components/templates/Page'
 import Main from 'components/templates/Main'
 
+import styles from './Home.module.scss'
+
 const Home = () => (
   <Page data-testid="library-screen">
     <Main>
       <header>
-        <h1>Home</h1>
+        <h1 className={styles.h1}>Home</h1>
       </header>
     </Main>
   </Page>
