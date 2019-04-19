b1df0988b326684a1b6a9ec43b40fe713c48deab
Hanna Söderström <gothbarbie84@gmail.com>
2019-04-19 15:37:51 +0200

    feat: move

--- src/components/templates/Main/Main.js
+++ src/components/templates/Main/Main.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import styles from './Main.module.scss'
+
 /**
  *  Use this component for shared styling, like content padding
  */
 
-const Main = ({ children }) => <div>{children}</div>
+const Main = ({ children }) => <div className={styles.main}>{children}</div>
 
 export default Main
