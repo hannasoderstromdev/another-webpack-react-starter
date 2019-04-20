567dc8cddbc3704e14da460079b69ac9142547c6
Hanna Söderström <gothbarbie84@gmail.com>
2019-04-20 08:52:31 +0200

    feat: use jest-localstorage-mock

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,4 +4,7 @@ import 'jest-dom/extend-expect'
 // Cleans up DOM after each test automatically
 import 'react-testing-library/cleanup-after-each'
 
+// Mock for local-storage
+import 'jest-localstorage-mock'
+
 import './fontAwesome'
